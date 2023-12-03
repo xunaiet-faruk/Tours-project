7b04f98ed4ce4bd8181b736253d3ad4f141cdcb8
xunaiet <xunaiet28@gmail.com>
2023-12-04 00:00:45 +0600

    product card added

--- src/Pages/Shop/Shops.jsx
+++ src/Pages/Shop/Shops.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 
 
 const Shops = () => {
+
+    const [shop,setShop] =useState([])
+    useEffect(()=>{
+        fetch('/Product.json')
+        .then(res => res.json())
+        .then(data => setShop(data))
+
+    })
+
+
     return (
         <div>
             <div className="hero min-h-screen mb-12" style={{ backgroundImage: 'url(https://i.ibb.co/D4mJW55/image.png)' }}>
@@ -13,6 +24,25 @@ const Shops = () => {
                     </div>
                 </div>
             </div>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 max-w-screen-xl mx-auto">
+                {
+
+
+                    shop.map(item => <div key={item._id} className="card card-compact bg-base-100 shadow-xl">
+                        <figure><img src={item?.image} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{item?.name}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>)
+
+
+                }
+            </div>
+
         </div>
     );
 };
